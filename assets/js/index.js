ac1342a12311c3b17695163a26f88e3dc6d1754e
Eric Marshall <emm035@bucknell.edu>
2016-07-12 14:43:16 -0400

    New site update

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function () {
+  // Showing/Hiding experience/education items
+  var showExpEdu = 3;
+  if ($(".exp-edu").length <= showExpEdu) {
+    $("#show-exp-edu").hide();
+    $("#hide-exp-edu").hide();
+  } else {
+    $("#hide-exp-edu").hide();
+    $(".exp-edu").slice(showExpEdu, $(".exp-edu").length).hide();
+    $("#show-exp-edu").on('click', function (e) {
+      e.preventDefault();
+      $(".exp-edu:hidden").slideDown();
+      $("#show-exp-edu").hide();
+      $("#hide-exp-edu").show();
+    });
+    $("#hide-exp-edu").on('click', function (e) {
+      e.preventDefault();
+      $(".exp-edu").slice(showExpEdu, $(".exp-edu").length).slideUp();
+      $("#hide-exp-edu").hide();
+      $("#show-exp-edu").show();
+    });
+  }
+  // Showing/Hiding project items
+  var showProjects = 3;
+  if ($(".proj").length <= showProjects) {
+    $("#show-proj").hide();
+    $("#hide-proj").hide();
+  } else {
+    $("#hide-proj").hide();
+    $(".proj").slice(showProjects, $(".proj").length).hide();
+    $("#show-proj").on('click', function (e) {
+      e.preventDefault();
+      $(".proj:hidden").slideDown();
+      $("#show-proj").hide();
+      $("#hide-proj").show();
+    });
+    $("#hide-proj").on('click', function (e) {
+      e.preventDefault();
+      $(".proj").slice(showProjects, $(".proj").length).slideUp();
+      $("#hide-proj").hide();
+      $("#show-proj").show();
+    });
+  }
+});
